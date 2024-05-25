d03be96488aee731400699167542760c21479704
Uthaeus <romanlavery@gmail.com>
2024-05-25 11:58:53 -0600

    successfully displaying users in admin users chart, navigating to user detail

--- src/components/admin/admin-chart/admin-users-chart-item.js
+++ src/components/admin/admin-chart/admin-users-chart-item.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function AdminUsersChartItem({ user }) {
     return (
-        <Link to={`/admin/users/${user.id}`} className="admin-users-chart-item">
+        <Link to={`/admin/user/${user.id}`} className="admin-users-chart-item">
             <p className="admin-users-chart-item-title">{user.name}</p>
             <p className="admin-users-chart-item-title">{user.email}</p>
         </Link>
