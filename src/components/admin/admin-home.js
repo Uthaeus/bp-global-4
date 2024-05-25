b720a651785d822e5e7c46423f9c9602d4d7eaee
Uthaeus <romanlavery@gmail.com>
2024-05-24 19:16:13 -0600

    added create new user button to admin home, added back button to new user, added styling

--- src/components/admin/admin-home.js
+++ src/components/admin/admin-home.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom"
 
 export default function AdminHome() {
     return (
         <div className="admin-container">
             <h1 className="admin-title">Admin Home</h1>
+            <p className="w-75 d-flex justify-content-end">
+                <Link to='/admin/user/new' className="btn btn-primary">Create New User</Link>
+            </p>
         </div>
     )
 }
--- src/components/admin/admin-users/admin-new-user.js
+++ src/components/admin/admin-users/admin-new-user.js
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 
 import AdminUserForm from "./admin-user-form";
 
 export default function AdminNewUser() {
-
-    const onSubmit = async (data) => {
-        console.log(data);
-    }
 
     return (
         <div className="admin-container">
             <h1 className="admin-title">Create New User</h1>
 
             <AdminUserForm />
+
+            <div className="admin-container-actions">
+                <Link to='/admin' className="btn btn-primary">Back</Link>
+            </div>
         </div>
     )
 }
