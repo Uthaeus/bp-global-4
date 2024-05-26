1036053eebbc672ff405db8f0696d373a41b2bd3
Uthaeus <romanlavery@gmail.com>
2024-05-25 20:09:35 -0600

    created add and remove image methods for order form, added styling

--- src/components/admin/admin-orders/admin-order-form.js
+++ src/components/admin/admin-orders/admin-order-form.js
@@ -1,15 +1,27 @@
 import { useForm } from "react-hook-form"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function AdminOrderForm({ order }) {
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const [orderImages, setOrderImages] = useState([]);
 
     useEffect(() => {
         if (order) {
             reset(order);
         }
     }, [order, reset]);
+
+    const addImageHandler = (event) => {
+
+        // create url
+        const url = URL.createObjectURL(event.target.files[0]);
+        setOrderImages([...orderImages, url]);
+    }
+
+    const removeImageHandler = (img) => {
+        setOrderImages(orderImages.filter((image) => image !== img));
+    }
 
     const onSubmit = (data) => {
         console.log(data);
@@ -28,6 +40,22 @@ export default function AdminOrderForm({ order }) {
                 <label htmlFor="order_number">Order Number</label>
                 <input type="text" id="order_number" className="form-control" {...register("order_number", { required: true })} />
                 {errors.order_number && <span className="text-danger">Order Number is required</span>}
+            </div>
+
+            <div className="form-group mb-3">
+                <label htmlFor="image">Add Image</label>
+
+                <input type="file" id="image" className="form-control" onChange={addImageHandler} />
+            </div>
+
+            <div className="order-form-images">
+                {orderImages.map((image, index) => (
+                    <div key={index} className="order-form-image-wrapper">
+                        <img src={image} alt="order image" className="order-form-image" />
+
+                        <button type="button" className="btn btn-danger order-form-image-remove" onClick={() => removeImageHandler(image)}>X</button>
+                    </div>
+                ))}
             </div>
 
             <button type="submit" className="btn btn-primary">{ order ? "Update Order" : "Create Order"}</button>
