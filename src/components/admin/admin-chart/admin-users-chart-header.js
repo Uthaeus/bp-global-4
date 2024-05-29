77bc30813f6ab5db9cfbcce7d0c138ea58df143d
Uthaeus <romanlavery@gmail.com>
2024-05-28 19:13:31 -0600

    refactoring styling for admin charts

--- src/components/admin/admin-chart/admin-users-chart-header.js
+++ src/components/admin/admin-chart/admin-users-chart-header.js
@@ -2,7 +2,7 @@
 export default function AdminUsersChartHeader() {
 
     return (
-        <div className="admin-users-chart-header">
+        <div className="admin-chart-header admin-users-chart-header">
             <div className="admin-users-chart-header-item">
                 <p className="admin-users-chart-header-item-title">Customer</p>
             </div>
