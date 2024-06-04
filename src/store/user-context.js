bb63aa66373209f64729c517da5a569a2cd5f34d
Uthaeus <romanlavery@gmail.com>
2024-06-04 16:14:11 -0600

    implemented firebase methods in user context

--- src/store/user-context.js
+++ src/store/user-context.js
@@ -1,13 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 
-import { dummyAdmin } from "./dummy/dummy-users";
-// import { dummyUser } from "./dummy/dummy-users";
+import { doc, getDoc, onSnapshot, setDoc, updateDoc, collection } from "firebase/firestore";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+
+import { db, auth } from "../firebase";
+
 
 export const UserContext = createContext({
     user: null,
     isAdmin: false,
-    setUser: () => {},
-    setIsAdmin: () => {},
     updateUser: () => {},
     logOutUser: () => {}
 });
@@ -18,17 +19,39 @@ const UserContextProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        setUser(dummyAdmin);
-        setIsAdmin(true);
-        setIsLoading(false);
+        const unsubscribe = onAuthStateChanged(auth, initializeUser);
+        return () => unsubscribe();
     }, []);
+
+    const initializeUser = async (user) => {
+
+        if (user) {
+            const docRef = doc(db, 'users', user.uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                const userData = docSnap.data();
+                setUser({...userData, id: user.uid});
+                if (userData.role === 'admin') {
+                    setIsAdmin(true);
+                } else {
+                    setIsAdmin(false);
+                }
+            }
+        } else {
+            setUser(null);
+            setIsAdmin(false);
+        }
+        setIsLoading(false);
+    }
 
     const updateUser = (data) => {
         console.log('update user method - user-context', data);
         setUser({...user, ...data});
+        updateDoc(doc(db, 'users', user.id), data);
     }
 
     const logOutUser = () => {
+        signOut(auth);
         setUser(null);
         setIsAdmin(false);
     }
